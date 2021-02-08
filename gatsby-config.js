e92a4f098d5a81626591ddd67508017e085b4c4a
Wojciech Bożek <bozek.wojciech@gmail.com>
2021-02-08 18:56:20 +0100

    PWA gatsby page

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,5 +48,18 @@ module.exports = {
       resolve: "gatsby-source-contentful",
       options: contentfulConfig,
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/goal.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 };
